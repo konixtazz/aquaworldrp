04d5f4983f1a133539b8843ae19020218f2f9372
konixtazz <delperier.tommy@gmail.com>
2026-01-30 17:39:58 +0100

    Add license verification functionality
    Implement license verification for FiveM scripts.

---
+++ verify_license.php
@@ -0,0 +1,20 @@
+<?php
+header('Content-Type: application/json');
+
+// Ta liste de licences autorisées (ou connecte ça à une base de données SQL)
+$allowed_licenses = [
+    "cfx-12345-votre-client-1",
+    "cfx-67890-votre-client-2"
+];
+
+// Récupérer la licence envoyée par le script FiveM
+$license_check = $_GET['license'] ?? '';
+
+if (in_array($license_check, $allowed_licenses)) {
+    // Si la licence est dans la liste
+    echo json_encode(["status" => "AUTHORIZED", "token" => md5(time())]);
+} else {
+    // Si la licence n'est pas connue
+    echo json_encode(["status" => "DENIED"]);
+}
+?>
